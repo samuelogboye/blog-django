c555dad7d8bd807b3d1b6bfc0a0554852c139374
Samuel Ogboye <ogboyesam@gmail.com>
2023-10-29 06:16:26 +0100

    more

--- myblog/models.py
+++ myblog/models.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import User
 from django.urls import reverse
 from ckeditor.fields import RichTextField
 from django.utils import timezone
-# from django.utils.text import slugify
+# from django.utils.text import slugif
 
 
 
